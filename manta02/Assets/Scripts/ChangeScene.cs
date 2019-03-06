0064d9d1fd2311a319f65f45b629f1b48015daea
Hong <khj908117@naver.com>
2019-03-06 16:12:23 +0900

    hong

---
+++ manta02/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void SceneChange1()
+    {
+        SceneManager.LoadScene("RastScene");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("");
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- manta02/Assets/Scripts/InGameUIControl.cs
+++ manta02/Assets/Scripts/InGameUIControl.cs
@@ -10,19 +10,27 @@ public class InGameUIControl : MonoBehaviour
 
     //인게임 UI를 총괄하는 스크립트
 
-    private InGameUIControl(){}
+    private InGameUIControl() { }
     private static InGameUIControl instance;
 
-    public GameObject selectionPanel, itemSelectionPanel, dialogPanel, infoPanel, slot1, slot2, 
+    public GameObject selectionPanel, itemSelectionPanel, dialogPanel, infoPanel, slot1, slot2,
         extinguisher, sand, alarm, multitap, gasnozzle, towel;
-   
+
     public Text dialog, consol, info;
     public Sprite extinguisherImg, sandImg, alarmImg, multitapImg, towelImg;
     bool isMissonEnd, isGameEnd;
 
+    public Text timeLable;
+    [HideInInspector] public float timeCount = 0f;
+    [HideInInspector] public int min = 0;
+
     [HideInInspector] public GameObject clickedItem;
 
     List<Slot> slots;
+
+    public Slider hpSlider;
+    public int hp = 50;
+    public int currentHP = 25;
 
 
     GraphicRaycaster m_Raycaster;
@@ -40,12 +48,20 @@ public class InGameUIControl : MonoBehaviour
         return instance;
     }
 
-   
+
 
     // Start is called before the first frame update
     void Start()
-    {   
+    {
         //게임이 시작되면
+        //타이머
+        timeCount = 0f;
+        min = 3;
+
+
+        //체력바
+        currentHP = hp;
+
         dialog.text = "어디선가 불이 났습니다! 원인을 찾아보세요.";
         dialogPanel.SetActive(true);
         isMissonEnd = isGameEnd = false;
@@ -62,7 +78,32 @@ public class InGameUIControl : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+
+
+
+        hpSlider.maxValue = hp;
+
+        hpSlider.value = currentHP;
+
+
+
+        //TImer를 위한 
+        if (timeCount >= 0)
+        {
+            timeCount -= Time.deltaTime;
+            timeLable.text = string.Format("{0:D2} : {1:D2}", min, (int)timeCount);
+            if (timeCount < 0)
+            {
+                min -= 1;
+                timeCount += 60;
+            }
+        }
+        if (min < 0)
+        {
+            timeCount = 0f;
+        }
+
         //게임오브젝트에 광선을 쏴서 식별한다.(매 프레임마다 마우스클릭이 있는지 검사한다)
         if (!Input.GetMouseButtonDown(0))
         { return; }
@@ -70,11 +111,11 @@ public class InGameUIControl : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //메인카메라 태그 설정해야 nullponit exception 안난다.
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
-        {   
+        {
             //인스펙터에서 각 아이템과 아이템이미지에 콜라이더가 있는지, 태그가 잘 되있는지, 이름이 잘 되있는지 확인!
 
             //아이템을 클릭했을 때
-            if(hit.collider.tag == "item")
+            if (hit.collider.tag == "item")
             {
                 Debug.Log("ActivateSelectionPanel");
                 selectionPanel.SetActive(true);
@@ -82,27 +123,27 @@ public class InGameUIControl : MonoBehaviour
 
                 //클릭된 게임 오브젝트트를 검사, 아이템의 정보를 인포패널에 적어둠
 
-                if(clickedItem.name == extinguisher.name)
+                if (clickedItem.name == extinguisher.name)
                 {
                     info.text = "소화기다.";
                 }
-                else if(clickedItem.name == sand.name)
+                else if (clickedItem.name == sand.name)
                 {
                     info.text = "모래다.";
                 }
-                else if(clickedItem.name == alarm.name)
+                else if (clickedItem.name == alarm.name)
                 {
                     info.text = "비상벨이다";
                 }
-                else if(clickedItem.name == multitap.name)
+                else if (clickedItem.name == multitap.name)
                 {
                     info.text = "멀티탭이다";
                 }
-                
+
             }
-           
+
             //클릭된 게임오브젝트의 태그가 mission이거나, 미션이 끝났거나, 게임이 끝났으면(아직 구현 안함)
-            if(hit.collider.tag == "mission" || isMissonEnd || isGameEnd)
+            if (hit.collider.tag == "mission" || isMissonEnd || isGameEnd)
             {
                 Debug.Log("ActiveDialogPanel");
                 dialogPanel.SetActive(true);
@@ -122,7 +163,7 @@ public class InGameUIControl : MonoBehaviour
                 Debug.Log("ActiveitemSelectioPanel");
                 itemSelectionPanel.SetActive(true); //아니면 아이템셀렉션패널을 연다
                 clickedItem = result.gameObject; //클릭된 UI를 전역변수로 넘겨준다.
-                
+
                 //클릭된 슬롯의 이미지를 검사해 케이스마다 아이템 정보창의 컨텐츠를 셋팅한다.
                 if (result.gameObject.GetComponent<Image>().sprite == extinguisherImg)
                 {
@@ -136,28 +177,23 @@ public class InGameUIControl : MonoBehaviour
         }
 
 
-
-       
-
-
-
     }
-   // 정보보기 버튼을 누르면 infoPanel 띄우기. 정보보기 버튼에 연결해야함.
+    // 정보보기 버튼을 누르면 infoPanel 띄우기. 정보보기 버튼에 연결해야함.
     public void ActiveInfoPanel()
     {
         Debug.Log("ActiveInfoPanel");
         infoPanel.SetActive(true);
-        
+
     }
-    
+
 
     public void Pick()
     {
-        Debug.Log("클릭된 아이템 이름 = "+clickedItem.name);
+        Debug.Log("클릭된 아이템 이름 = " + clickedItem.name);
 
 
         if (clickedItem.GetComponent<Item>().itemType != Item.Type.Equipment) { dialog.text = "습득할 수 없는 아이템입니다"; dialogPanel.SetActive(true); return; }
-        for(int i =0; i<slots.Count; i++)
+        for (int i = 0; i < slots.Count; i++)
         {
             //Slot스크립트의 isEmpty를 받아온다.(객체의 상태에 접근해 참조)
             if (slots[i].isEmpty == true) //슬롯이 비어있고, 아이템이 장착할 수 있는 타입이면
@@ -168,7 +204,7 @@ public class InGameUIControl : MonoBehaviour
             }
             else
             {
-                Debug.Log(i+"번째 슬롯이 꽉참");
+                Debug.Log(i + "번째 슬롯이 꽉참");
             }
 
         }
@@ -185,7 +221,7 @@ public class InGameUIControl : MonoBehaviour
     {
         if (clickedItem != null && clickedItem.GetComponent<Image>() != null && clickedItem.GetComponent<Image>().sprite != null)
         {
-            clickedItem.GetComponent<Image>().color = new Color32(0,0,0,150);
+            clickedItem.GetComponent<Image>().color = new Color32(0, 0, 0, 150);
             clickedItem.GetComponent<Image>().sprite = null;
         }
     }
@@ -193,8 +229,8 @@ public class InGameUIControl : MonoBehaviour
     public void UseItem()
     {
         //아이템 타입이 Misc이면 
-        if (clickedItem.GetComponent<Item>().itemType == Item.Type.Misc) { dialog.text = "사용할 수 없는 아이템입니다"; dialogPanel.SetActive(true); return; } 
-        Debug.Log("사용하는 아이템:"+clickedItem.name);
+        if (clickedItem.GetComponent<Item>().itemType == Item.Type.Misc) { dialog.text = "사용할 수 없는 아이템입니다"; dialogPanel.SetActive(true); return; }
+        Debug.Log("사용하는 아이템:" + clickedItem.name);
         //아이템을 습득하지 않고 바로 사용할 경우
         if (clickedItem != null)
         {
@@ -216,8 +252,8 @@ public class InGameUIControl : MonoBehaviour
                 GameObject sand = GameObject.Find("sand");
                 sand.GetComponent<Shooter>().ActivateShooter();
             }
-            
-            
+
+
         }
         //아이템을 습득하고나서 사용할 경우
         if (clickedItem != null && clickedItem.GetComponent<Image>() != null && clickedItem.GetComponent<Image>().sprite != null)
@@ -230,13 +266,13 @@ public class InGameUIControl : MonoBehaviour
                 GameObject extinguisher = GameObject.Find("extinguisher"); //이름으로 하지말까..하지만 방법이..??
                 extinguisher.GetComponent<Shooter>().ActivateShooter();
             }
-            if(clickedItem.GetComponent<Image>().sprite == alarmImg)
+            if (clickedItem.GetComponent<Image>().sprite == alarmImg)
             {
                 Debug.Log("알람이 울립니다");
-                GameObject alarm = GameObject.Find("alarm"); 
+                GameObject alarm = GameObject.Find("alarm");
                 alarm.GetComponent<AudioSource>().Play();
             }
-            if(clickedItem.GetComponent<Image>().sprite == sandImg)
+            if (clickedItem.GetComponent<Image>().sprite == sandImg)
             {
 
             }
